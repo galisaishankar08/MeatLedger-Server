e973f5106034c70693bffc5f570fdb47a966538d
galisaishankar08 <2010030054@klh.edu.in>
2023-03-13 19:52:04 +0530

    MongoDB initialized

--- server.js
+++ server.js
@@ -1,17 +1,32 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require("cors");
+const cors = require('cors');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const app = express();
-const port = 3001
+const port = 3001;
 
 app.use(cors());
 app.use(bodyParser.json());
 
-app.listen(port, function () {
-    console.log(`Server listening on port ${port}`)
+mongoose.set('strictQuery', true);
+mongoose.connect(process.env.MONGO_CONNECTION_STRING, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => {
+  console.log('MongoDB connected');
+  app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+  });
+})
+.catch((error) => {
+  console.error('Error connecting to MongoDB:', error.message);
 });
 
-app.get("/", function(req, res) {
-    res.send("Hi")
-})
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
