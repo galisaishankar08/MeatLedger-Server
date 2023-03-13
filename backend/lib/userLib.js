ccfe987229e22ff883b1593ef81f5604073951b0
galisaishankar08 <2010030054@klh.edu.in>
2023-03-13 20:36:32 +0530

    UserModel name changed

--- backend/lib/userLib.js
+++ backend/lib/userLib.js
@@ -1,4 +1,4 @@
-const userModel = require('../models/userModel');
+const userModel = require('../models/UserModel');
 
 module.exports.getAllUsers = async function(){
     return await userModel.find({});
