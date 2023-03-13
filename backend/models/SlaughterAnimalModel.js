d417bbedee9787c247715645710230270066e1e2
galisaishankar08 <2010030054@klh.edu.in>
2023-03-13 20:31:41 +0530

    MongoDB Model Validation

--- backend/models/SlaughterAnimalModel.js
+++ backend/models/SlaughterAnimalModel.js
@@ -1,25 +1,28 @@
 const mongoose = require('mongoose')
 
-const SlaughterAnimalSchema = new mongoose.Schema(
-    {
-        "preslaughterhandling":{type: String, require:true},
-        "stunning": {type: String, required: true},
-        "slaughtering": {type: String, required: true},
-        "plantcode": {type: String, required: true},
-        "plantcountry": {type: String, required: true},
-        "age": {type: String, required: true},
-        "weight": {type: String, required: true},
-        "halal": {type: Boolean, required: true},
-        "wastage": {type: String, required: true},
-        "storagetemperature": {type: String, required: true},
-        "meattype": {type: String, required: true},
-        "expirydate": {type: Date, required: true},
-        "packagedate": {type: Date, required: true},
-        "packingmode": {type: String, required: true},
-        "certified": {type: String, required: true},
-        "companydetails": {type: String, required: true},
-        "address": {type: String, required: true},
-    }
-)
-
-module.exports = mongoose.model("SlaughterAnimal", SlaughterAnimalSchema);
+if (!mongoose.models['SlaughterAnimal']) {
+    const SlaughterAnimalSchema = new mongoose.Schema(
+        {
+            "preslaughterhandling":{type: String, require:true},
+            "stunning": {type: String, required: true},
+            "slaughtering": {type: String, required: true},
+            "plantcode": {type: String, required: true},
+            "plantcountry": {type: String, required: true},
+            "age": {type: String, required: true},
+            "weight": {type: String, required: true},
+            "halal": {type: Boolean, required: true},
+            "wastage": {type: String, required: true},
+            "storagetemperature": {type: String, required: true},
+            "meattype": {type: String, required: true},
+            "expirydate": {type: Date, required: true},
+            "packagedate": {type: Date, required: true},
+            "packingmode": {type: String, required: true},
+            "certified": {type: String, required: true},
+            "companydetails": {type: String, required: true},
+            "address": {type: String, required: true},
+        }
+    )
+    module.exports = mongoose.model("SlaughterAnimal", SlaughterAnimalSchema);
+} else {
+  module.exports = mongoose.model('SlaughterAnimal')
+}
--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -1,16 +1,19 @@
 const mongoose = require('mongoose')
 
-const userSchema = new mongoose.Schema(
-    {
-        "email": {type: String, unique: true, required: true},
-        "name": {type: String, required: true},
-        "password": {type: String, required: true},
-        "contact": {type: String, required: true},
-        "address": {type: String, required: true},
-        "usertype": {type: String, required: true},
-        "verified": {type: Boolean,default:false, required: true},
-        "joinedAt": {type: Date, default: Date.now()}
-    }
-)
+// Check if the model has already been defined before defining it
+if (!mongoose.models['user']) {
+  const userSchema = new mongoose.Schema({
+    "email": {type: String, unique: true, required: true},
+    "name": {type: String, required: true},
+    "password": {type: String, required: true},
+    "contact": {type: String, required: true},
+    "address": {type: String, required: true},
+    "usertype": {type: String, required: true},
+    "verified": {type: Boolean,default:false, required: true},
+    "joinedAt": {type: Date, default: Date.now()}
+  })
 
-module.exports = mongoose.model('user', userSchema);
+  module.exports = mongoose.model('user', userSchema)
+} else {
+  module.exports = mongoose.model('user')
+}
